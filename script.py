28e69f5e2a2d50f6d5528b0fb161995758337d2d
Kongxiao Zhao <freefrank21@gmail.com>
2024-07-26 04:07:28 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,54 +1,73 @@
 import requests
-import re
 
 def download_m3u(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.text
 
-def process_m3u(m3u_content):
-    lines = m3u_content.splitlines()
-    channel_dict = {}
-    current_channel = None
-
-    for line in lines:
+def process_m3u(content):
+    channels = {}
+    output_lines = []
+    lines = content.split('\n')
+    
+    i = 0
+    while i < len(lines):
+        line = lines[i].strip()
+        if not line:
+            i += 1
+            continue
         if line.startswith('#EXTINF'):
-            current_channel = line
-            match = re.search(r'tvg-name="([^"]+)"', line)
-            if match:
-                channel_name = match.group(1)
-                if channel_name not in channel_dict:
-                    channel_dict[channel_name] = {'hevc': None, 'normal': None, 'fps': None}
-        elif line.startswith('http') and current_channel:
-            match = re.search(r'tvg-name="([^"]+)"', current_channel)
-            if match:
-                channel_name = match.group(1)
-                if 'HEVC' in channel_name:
-                    channel_dict[channel_name]['hevc'] = (current_channel, line)
-                elif '50 FPS' in channel_name:
-                    channel_dict[channel_name]['fps'] = (current_channel, line)
-                else:
-                    channel_dict[channel_name]['normal'] = (current_channel, line)
-            current_channel = None
+            tvg_id = get_tvg_id(line)
+            quality = get_quality(line)
+            
+            if tvg_id not in channels:
+                channels[tvg_id] = (line, lines[i + 1].strip(), quality)
+            else:
+                if quality_priority(quality) > quality_priority(channels[tvg_id][2]):
+                    channels[tvg_id] = (line, lines[i + 1].strip(), quality)
+            i += 1
+        else:
+            output_lines.append(line)
+        i += 1
 
-    new_m3u_content = "#EXTM3U\n"
-    for channels in channel_dict.values():
-        if channels['hevc']:
-            new_m3u_content += f"{channels['hevc'][0]}\n{channels['hevc'][1]}\n"
-        elif channels['fps']:
-            new_m3u_content += f"{channels['fps'][0]}\n{channels['fps'][1]}\n"
-        elif channels['normal']:
-            new_m3u_content += f"{channels['normal'][0]}\n{channels['normal'][1]}\n"
+    for info in channels.values():
+        output_lines.append(info[0])
+        output_lines.append(info[1])
+        
+    return '\n'.join(output_lines)
 
-    return new_m3u_content
+def get_tvg_id(extinf_line):
+    start = extinf_line.find('tvg-id="') + len('tvg-id="')
+    end = extinf_line.find('"', start)
+    return extinf_line[start:end]
+
+def get_quality(extinf_line):
+    if 'HEVC' in extinf_line:
+        return 'HEVC'
+    elif '50 FPS' in extinf_line:
+        return '50 FPS'
+    else:
+        return 'NORMAL'
+
+def quality_priority(quality):
+    if quality == 'HEVC':
+        return 3
+    elif quality == 'NORMAL':
+        return 2
+    elif quality == '50 FPS':
+        return 1
+    else:
+        return 0
 
 def save_m3u(content, filename):
     with open(filename, 'w') as file:
         file.write(content)
 
-if __name__ == "__main__":
-    url = "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/itv_proxy.m3u"
-    m3u_content = download_m3u(url)
-    new_m3u_content = process_m3u(m3u_content)
-    save_m3u(new_m3u_content, 'processed_playlist.m3u')
-    print("Processed M3U playlist saved as 'processed_playlist.m3u'")
+def main():
+    url = 'https://raw.githubusercontent.com/vbskycn/iptv/master/tv/itv_proxy.m3u'
+    content = download_m3u(url)
+    processed_content = process_m3u(content)
+    save_m3u(processed_content, 'processed_itv_proxy.m3u')
+
+if __name__ == '__main__':
+    main()
